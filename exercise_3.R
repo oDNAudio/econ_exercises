e1b2d8a3d0aff70374ea95d99a17b77c6dac356c
Nikolas Kuschnig <kuschnignikolas@gmail.com>
2018-05-26 14:53:24 +0200

    reworked ssvs function (output etc) created charts and comparison

--- exercise_3.R
+++ exercise_3.R
@@ -1,4 +1,6 @@
 library(BMS)
+library(ggplot2)
+library(ggthemes)
 
 source("exercise_3_ssvs.R")
 
@@ -9,7 +11,7 @@ X = datafls[, 2:ncol(datafls)]
 rm(datafls)
 
 tau0s = c(1, 1e-2, 1e-5, 1e-15)
-tau1_scales = c(100, 1000, 10000)
+tau1_scales = c(100, 1000)
 
 pips = vector("list", length(tau0s) * length(tau1_scales))
 
@@ -32,3 +34,65 @@ for(scale in tau1_scales) {
     i = i + 1
   }
 }
+
+pips_auto = vector("list", 1)
+pips_auto = ssvs(Y, X)[[1]]
+
+# Demonstrate graphically
+
+df = data.frame(pips_z[[2]], pips_z[[4]], pips_z[[6]])
+df2 = data.frame(pips[[2]], pips[[4]], pips[[6]])
+names(df) = names(df2) = c("0.01(*100)", "1e-15(*100)", "0.01(*1000)")
+df$id = df2$id = as.numeric(rownames(df))
+
+p1 = ggplot(df, aes(x = id, y = `0.01(*100)`)) +
+  geom_point(aes(colour = "0.01(*100)")) +
+  geom_smooth(aes(colour = "0.01(*100)"), alpha = 0.2) +
+  geom_point(aes(y = `1e-15(*100)`, colour = "1e-15(*100)")) +
+  geom_smooth(aes(y = `1e-15(*100)`, colour = "1e-15(*100)"), alpha = 0.2) +
+  geom_point(aes(y = `0.01(*1000)`, colour = "0.01(*1000)")) +
+  geom_smooth(aes(y = `0.01(*1000)`, colour = "0.01(*1000)"), alpha = 0.2) +
+  coord_cartesian(ylim = c(0, 1), expand = 0) +
+  theme_fivethirtyeight() +
+  scale_color_gdocs(name = "")
+p1
+
+p2 = ggplot(df2, aes(x = id, y = `0.01(*100)`)) +
+  geom_point(aes(colour = "0.01(*100)")) +
+  geom_smooth(aes(colour = "0.01(*100)"), alpha = 0.2) +
+  geom_point(aes(y = `1e-15(*100)`, colour = "1e-15(*100)")) +
+  geom_smooth(aes(y = `1e-15(*100)`, colour = "1e-15(*100)"), alpha = 0.2) +
+  geom_point(aes(y = `0.01(*1000)`, colour = "0.01(*1000)")) +
+  geom_smooth(aes(y = `0.01(*1000)`, colour = "0.01(*1000)"), alpha = 0.2) +
+  coord_cartesian(ylim = c(0, 1), expand = 0) +
+  theme_fivethirtyeight() +
+  scale_color_gdocs(name = "")
+p2
+
+df3 = data.frame(pips_auto)
+names(df3) = "automatic"
+df3$id = as.numeric(rownames(df3))
+
+p3 = ggplot(df3, aes(x = id, y = automatic)) +
+  geom_point(aes(colour = "automatic")) +
+  geom_smooth(aes(colour = "automatic"), alpha = 0.2) +
+  coord_cartesian(ylim = c(0, 1), expand = 0) +
+  theme_fivethirtyeight() +
+  scale_color_gdocs(name = "")
+p3
+
+df4 = data.frame(pips_auto, pips_z[[2]], pips[[2]])
+names(df4) = c("automatic", "standardised", "neither")
+df4$id = as.numeric(rownames(df4))
+
+p4 = ggplot(df4, aes(x = id, y = automatic)) +
+  geom_point(aes(colour = "automatic")) +
+  geom_smooth(aes(colour = "automatic"), alpha = 0.2) +
+  geom_point(aes(y = standardised, colour = "z, 0.01(*100)")) +
+  geom_smooth(aes(y = standardised, colour = "z, 0.01(*100)"), alpha = 0.2) +
+  geom_point(aes(y = neither, colour = "no-z, 0.01(*100)")) +
+  geom_smooth(aes(y = neither, colour = "no-z, 0.01(*100)"), alpha = 0.2) +
+  coord_cartesian(ylim = c(0, 1), expand = 0) +
+  theme_fivethirtyeight() +
+  scale_color_gdocs(name = "")
+p4
--- exercise_3_ssvs.R
+++ exercise_3_ssvs.R
@@ -5,7 +5,7 @@
 #'
 #' @param Y The endogenous variable, must be convertible to a matrix.
 #' @param X The explanatory variables, must be convertible to a matrix.
-#' @param tau0 Number by which to scale an "unimportant" variable. Will use least squares estimate if not supplied.
+#' @param tau0 Number by which to scale an "unimportant" variable. Will use least squares estimates if not supplied.
 #' @param tau1 Number by which to scale an "important" variable. Defaults to 100 * tau0.
 #' @param save Iterations to be stored for calculating means.
 #' @param burn Iterations to be discarded before calculating means.
@@ -18,7 +18,7 @@
 
 ssvs = function(Y, 
                 X, 
-                tau0 = NULL,
+                tau0 = NULL, 
                 tau1 = tau0 * 100, 
                 save = 9000, 
                 burn = 1000, 
@@ -26,16 +26,18 @@ ssvs = function(Y,
                 S_prior = 0.01,
                 standardise = TRUE) {
   Y = matrix(Y)
-  X = as.matrix(cbind(rnorm(nrow(X), 0, 10), rnorm(nrow(X), 0, 1), X))
+  X = as.matrix(X)
 
   N = nrow(Y)
   K = ncol(X)
   
+  # Standardise (i.e. center and scale) the data if desired
   if(standardise) {
     Y = scale(Y)
     X = scale(X)
   }
   
+  # If no tau0 was supplied set up for OLS estimates, otherwise vectorise
   if(is.null(tau0)) {
     c0 = 0.1
     c1 = 10
@@ -44,28 +46,35 @@ ssvs = function(Y,
     tau1 = rep(tau1, K)
   }
   
+  # get OLS stuff
   OLS = solve(crossprod(X)) %*% crossprod(X, Y)
   SSE = as.numeric(crossprod(Y - X %*% OLS))
   sigma_draw = as.numeric(SSE / (N - K))
-  V_beta = sigma_draw * solve(crossprod(X))
+  V_beta_draw = sigma_draw * solve(crossprod(X))
   
+  # use OLS estimates for tau if no tau0 was supplied
   if(is.null(tau0)) {
-    tau0 <- c0 * sqrt(diag(V_beta))
-    tau1 <- c1 * sqrt(diag(V_beta))
+    tau0 <- c0 * sqrt(diag(V_beta_draw))
+    tau1 <- c1 * sqrt(diag(V_beta_draw))
   }
-
+  
+  # Set up gamma, and storage matrices
   gamma = matrix(1, K, 1)
   V_prior = diag(as.numeric(gamma * tau1 + (1 - gamma) * tau0))
 
   alpha_store = matrix(NA, save, K)
   sigma_store = matrix(NA, save, 1)
+  V_beta_store = matrix(NA, save, K)
   gamma_store = matrix(NA, save, K)
   
+  # Do the loop
   for(i in 1:(save + burn)) {
+    # Draw alpha
     V_post = solve(crossprod(X) / sigma_draw + diag(1 / diag(V_prior)))
     alpha_post = V_post %*% (crossprod(X, Y) / sigma_draw)
     alpha_draw = alpha_post + t(chol(V_post)) %*% rnorm(K)
     
+    # Determine inclusion based on alpha
     for(j in 1:K) {
       p0 = dnorm(alpha_draw[[j]], 0, sqrt(tau0[j]))
       p1 = dnorm(alpha_draw[[j]], 0, sqrt(tau1[j]))
@@ -74,27 +83,41 @@ ssvs = function(Y,
       gamma[[j]] = ifelse(p11 > runif(1), 1, 0)
     }
     
+    # Construct prior VC matrix
     V_prior = diag(as.numeric(gamma * tau1 + (1 - gamma) * tau0))
     
+    # Draw sigma^2
     S_post = S_prior + crossprod(Y - X %*% alpha_draw) / 2
     s_post = S_prior + N / 2
-    
     sigma_draw = 1 / rgamma(1, s_post, S_post)
+    V_beta_draw = diag(sigma_draw * solve(crossprod(X)))
     
+    # Ignore the first n(=burn) iterations, store results afterwards
     if(i > burn) {
       alpha_store[i - burn, ] = alpha_draw
       sigma_store[i - burn, ] = sigma_draw
+      V_beta_store[i - burn, ] = V_beta_draw
       gamma_store[i - burn, ] = gamma
     }
   }
   
+  # Get means for the output
   pip_mean = apply(gamma_store, 2, mean)
   alpha_mean = apply(alpha_store, 2, mean)
   sigma_mean = apply(sigma_store, 2, mean)
+  V_beta_mean = apply(V_beta_store, 2, mean)
   
-  out = list(pip_mean, alpha_mean, sigma_mean)
+  names(pip_mean) = names(alpha_mean) = names(V_beta_mean) = colnames(X)
   
-  names(out) = c("Posterior Inclusion Probability", "Posterior Mean", "Posterior Standard Deviation")
+  # Store the parameters used for the output
+  meta_data = list("tau0" = tau0, "tau1" = tau1,
+                   c("save" = save, "burn" = burn,
+                   "s_prior" = s_prior, "S_prior" = S_prior,
+                   "standardise" = standardise))
+  
+  out = list(pip_mean, alpha_mean, sigma_mean, V_beta_mean, meta_data)
+  
+  names(out) = c("pip", "post_mean", "post_std", "post_var_beta", "meta")
 
   return(out)
 }
